adf5800b6ea7ecdf57d965af8905ffbc9539aa27
Moksleivis <Moksleivis@kitm.local>
2023-01-19 10:05:19 +0200

    Added hitboxes (doesn't work as intended) and a death screen

--- javascript/movement.js
+++ javascript/movement.js
@@ -6,38 +6,78 @@ let running = true;
 
 let mid_air = false;
 
-const JUMP_HEIGHT = 70;
+const JUMP_HEIGHT = 150; //Bigger = Higher
 
-const JUMP_SPEED = 0.005;
+const JUMP_SPEED = 0.00000000000000000000000000000000001; //Lower = Faster
 
-const MOVEMENT_SPEED = 3;
+const MOVEMENT_SPEED = 5; //Lower = Faster
 
 const stop = document.getElementById("stop");
-    //TODO:Add hitboxes, using an array, check if a collision happened by checking if the obstacle's rightmost coordinate is at the hitbox
-//Function//
+
+function death_screen(){
+    const frame = document.getElementById("frame");
+    frame.style.backgroundColor = "#121212";
+    const death_text = document.createElement("div");
+    death_text.classList.add("death");
+    death_text.appendChild(document.createTextNode("Massive Skill Issue"));
+    frame.appendChild(death_text);
+}
+
+function hitboxes(scrheight, dstat){
+    const player = document.getElementById("player");
+    const player_w = player.offsetWidth;
+    const player_h = player.offsetHeight;
+
+    const spike = document.getElementById("spiglys");
+
+    const pleft = parseInt(player.style.left);
+    const ptop = parseInt(player.style.bottom + player_h);
+    const pbot = parseInt(player.style.bottom);
+    const sleft = parseInt(getComputedStyle(spike).left);
+    const stop = parseInt(scrheight - parseInt((0 + spike.offsetHeight)));
+    const sbot = 0;
+    //FIXME: baD HITBOXES :()
+    if(!(pleft > sleft + spike.offsetWidth ||
+        pleft + player_w < sleft ||
+        ptop > stop + spike.offsetHeight ||
+        pleft + player_h < stop)){
+        //console.log("Rip Bozo");
+        //console.log(pleft, ptop, pbot, sleft, stop, sbot);
+        dstat.t = true;
+    }
+
+}
 async function main_loop(){
     await sleep(1000);
 
     const frame = document.getElementById("frame");
-
     let levellength = frame.offsetWidth-41;
-
+    let dead = {t: false};
     console.log(levellength);
-
+    const player = document.getElementById("player");
     let once = 0;
+    player.style.bottom = 0;
+    console.log(frame.offsetHeight - player.offsetHeight + player.bottom, frame.offsetHeight);
     while (once < 1){
-        if (running == false) break;
+        
+        if (running === false) {
+            break;
+        }
         console.log(running);
         for (let i = 0; i < levellength; ++i){
+            console.log("Death status", dead.t);
+            if (dead.t === true){
+                death_screen();
+                break;
+            }
             if (running === false) {
-                delete frame;
                 break;
             }
             await sleep(MOVEMENT_SPEED);
-            const player = document.getElementById("player");
             player.style.left = `${i}px`;
             frame.addEventListener("click", jump);
             stop.addEventListener("click", stopTheLoop);
+            hitboxes(frame.offsetHeight, dead);
             }
             ++once;
         }
@@ -48,6 +88,7 @@ function stopTheLoop(){
     stop.innerHTML = "Stopped :)";
     running = false;
 }
+
 async function jump(){
     const player = document.getElementById("player");
     if (mid_air === true) {
